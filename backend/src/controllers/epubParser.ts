b25f4da188be76b9380feb97cb623d37c095d296
Nicolas Klein <141509949+nicolaskleinaraujo@users.noreply.github.com>
2024-11-16 19:41:13 -0300

    hotfixes the epubParser controller

--- backend/src/controllers/epubParser.ts
+++ backend/src/controllers/epubParser.ts
@@ -1,27 +1,35 @@
 import path from "node:path"
 import fs from "node:fs"
+import iconv from "iconv-lite"
 import Epub, { Options } from "epub-gen"
 
 const epubParser = async() => {
-    const txtFilePath = path.resolve("../teste.txt")
-    const epubFilePath = path.resolve("../teste.epub")
-    
-    const content = fs.readFileSync(txtFilePath, "utf8")
-
-    const options: Options = {
-        title: "Newsletter",
-        author: "Filipe Deschamps",
-        content: [
-            {
-                title: "Capitulo 1",
-                data: content
-            }
-        ]
-    }
-
     try {
-        new Epub(options, epubFilePath)
-        console.log("Parsed")
+        const txtFilePath = path.resolve("../teste.txt")
+        const tocTemplatePath = path.resolve("./src/templates/toc.xhtml")
+        console.log(tocTemplatePath)
+
+        const rawText = fs.readFileSync(txtFilePath)
+        const decodedText = iconv.decode(rawText, "utf-8")
+    
+        const options: Options = {
+            title: "Newsletter",
+            author: "Filipe Deschamps",
+            content: [
+                {
+                    data: decodedText,
+                    beforeToc: true,
+                    excludeFromToc: true,
+                }
+            ],
+            lang: "pt",
+            verbose: false,
+            customHtmlTocTemplatePath: tocTemplatePath,
+        }
+
+        new Epub(options, "../teste.epub").promise.then(() => {
+            console.log("Parsed")
+        })
     } catch (error) {
         console.log(error)
     }
