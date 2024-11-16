b7e9ac04bdda35586ce4afe33fce253601b61e5a
Nicolas Klein <141509949+nicolaskleinaraujo@users.noreply.github.com>
2024-11-16 19:41:35 -0300

    hotfixes the decoder and encoder

--- backend/src/controllers/formatFile.ts
+++ backend/src/controllers/formatFile.ts
@@ -1,15 +1,24 @@
 import fs from "node:fs"
 import path from "node:path"
+import iconv from "iconv-lite"
 
 const formatFile = async() => {
     try {
         const filePath = path.resolve("../teste.txt")
-        const file = fs.readFileSync(filePath, "utf8")
+        const rawFile = fs.readFileSync(filePath)
+        let file = iconv.decode(rawFile, "windows-1252")
+
+        file = file.replace(
+            /(https?:\/\/[^\s]+)/g,
+            (match) => `[${match.match(/(?:www\.|)(\w+\.\w+)/)?.[0]}](${match})`
+        )
 
         const regex = /(From: [\s\S]*?Subject: [\s\S]*?(\r?\n)+)|Cancelar inscri��o\s*\(\s*https?:\/\/[^\)]+\)\s*\|\s*Indicar Newsletter\s*\(\s*https?:\/\/[^\)]+\)/g
 
-        const newFile = file.replace(regex, '')
-        fs.writeFileSync(filePath, newFile)
+        file = file.replace(regex, '')
+
+        const encodedFile = iconv.encode(file, "utf-8")
+        fs.writeFileSync(filePath, encodedFile)
 
         console.log("formated")
     } catch (error) {
