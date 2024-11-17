188819af448bc0578374411f77cb89ce167ac4c5
Nicolas Klein <141509949+nicolaskleinaraujo@users.noreply.github.com>
2024-11-17 13:17:51 -0300

    hotfixes the env value

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -11,29 +11,25 @@ const resend = new Resend(process.env.RESEND_KEY)
 const sendNewsLetter = async() => {
     const date = new Date()
 
-    const email: string | undefined = process.env.EMAIL_TO
-
     const filePath: string = path.resolve("../teste.epub")
     const attachment = fs.readFileSync(filePath).toString("base64")
 
-    if (email !== undefined) {
-        const { data, error } = await resend.emails.send({
-            from: "contatonkfa@nkportfolio.tech",
-            to: [email],
-            subject: "Convert",
-            text: "Newsletter do dia",
-            attachments: [
-                {
-                    content: attachment,
-                    contentType: "application/epub+zip",
-                    filename: `Newsletter ${String(date.getDate()).padStart(2, "0")}/${String(date.getMonth() + 1).padStart(2, "0")}/${String(date.getFullYear()).slice(2)}.epub`
-                }
-            ]
-        })
+    const { data, error } = await resend.emails.send({
+        from: "contatonkfa@nkportfolio.tech",
+        to: [process.env.EMAIL_TO as string],
+        subject: "Convert",
+        text: "Newsletter do dia",
+        attachments: [
+            {
+                content: attachment,
+                contentType: "application/epub+zip",
+                filename: `Newsletter ${String(date.getDate()).padStart(2, "0")}/${String(date.getMonth() + 1).padStart(2, "0")}/${String(date.getFullYear()).slice(2)}.epub`
+            }
+        ]
+    })
 
-        console.log(data)
-        console.log(error)
-    }
+    console.log(data)
+    console.log(error)
 }
 
 //formatFile()
