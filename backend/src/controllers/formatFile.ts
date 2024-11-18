a620b92ff360d9876163dadbcb37022e84b2211c
Nicolas Klein <141509949+nicolaskleinaraujo@users.noreply.github.com>
2024-11-18 00:25:56 -0300

    changes txt files to html files

--- backend/src/controllers/formatFile.ts
+++ backend/src/controllers/formatFile.ts
@@ -8,18 +8,14 @@ const formatFile = async() => {
         const rawFile = fs.readFileSync(filePath)
         let file = iconv.decode(rawFile, "utf-8")
 
-        const headerRegex = /(?:^[-_]+[\r\n]+|^De:.*?[\r\n]+Enviado:.*?[\r\n]+Para:.*?[\r\n]+Assunto:.*?[\r\n]+)/gm
-        file = file.replace(headerRegex, "")
+        const nonHTML = /^[\s\S]*?(?=<html>)/
+        file = file.replace(nonHTML, "")
 
-        const footerRegex = /Cancelar inscrição[\s\S]*$/gm
-        file = file.replace(footerRegex, "")
+        const header = /<b>Para:<\/b>.*?<br>/i
+        file = file.replace(header, "")
 
-        file = file.replace(
-            /([^\s<]+)<(https?:\/\/[^\s>]+)>/g,
-            (_match, text, url) => `<a href="${url}" target="_blank" style="text-decoration: underline;">${text}</a>`
-        )
-
-        file = file.trim().split(/\r?\n\s*\r?\n/).map(paragraph => `<p>${paragraph.trim()}</p>`).join("\n\n")
+        const footer = /<a[^>]*>\s*(Cancelar\s*inscrição|Indicar\s*Newsletter)\s*<\/a>\s*\|?/g
+        file = file.replace(footer, "")
 
         const encodedFile = iconv.encode(file, "utf-8")
         fs.writeFileSync(filePath, encodedFile)
