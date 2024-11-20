74ccdee47a2d2165efeec33ae27670f654101d64
Nicolas Klein <141509949+nicolaskleinaraujo@users.noreply.github.com>
2024-11-19 22:49:49 -0300

    adds the controllers to the server

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,16 +1,23 @@
+// Modules
 import "dotenv/config"
-import express, { Application } from "express"
+import express, { Application, Request, Response } from "express"
 import cors from "cors"
 import cron from "node-cron"
 
+// Cron-Job Files
 import getNewsletter from "./config/imap"
 import formatFile from "./config/formatFile"
 import epubParser from "./config/epubParser"
 import sendNewsLetter from "./config/sendNewsletter"
 
+// Controllers
+import registerEmail from "./controllers/registerEmail"
+import deleteEmail from "./controllers/deleteEmail"
+
 const app: Application = express()
 const port: number = Number(process.env.PORT) || 3000
 
+app.use(express.json())
 app.use(
     cors({
         origin: process.env.ORIGIN_URL,
@@ -27,6 +34,9 @@ cron.schedule("0 15 * * 1-5", async() => {
         console.log("stoped")
     }
 })
+
+app.post("/email", (req: Request, res: Response) => registerEmail(req, res))
+app.delete("/email", (req: Request, res: Response) => deleteEmail(req, res))
 
 app.listen(port, () => {
     console.log(`Listening on port ${port}`)
