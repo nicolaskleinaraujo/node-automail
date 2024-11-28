74c3610c7fbb7b23089a736a47400d35f48213f3
Nicolas Klein <141509949+nicolaskleinaraujo@users.noreply.github.com>
2024-11-27 23:08:32 -0300

    finalizes the forceNewsletter controller

--- backend/src/controllers/forceNewsletter.ts
+++ backend/src/controllers/forceNewsletter.ts
@@ -1,5 +1,10 @@
+import "dotenv/config"
 import { Request, Response } from "express"
 import z from "zod"
+import getNewsletter from "../config/imap"
+import formatFile from "../config/formatFile"
+import epubParser from "../config/epubParser"
+import sendNewsLetter from "../config/sendNewsletter"
 
 interface Credential {
     password: string
@@ -9,9 +14,23 @@ const CredentialSchema = z.object({
     password: z.string().trim().max(150),
 })
 
-const forceNewsletter = async(req: Request, res: Response) => {
+const forceNewsletter = async(req: Request, res: Response): Promise<void> => {
     try {
         const { password }: Credential = CredentialSchema.parse(req.body)
+
+        const serverPassword = process.env.SERVER_PASSWORD
+
+        if (password !== serverPassword) {
+            res.status(401).json({ msg: "Senha incorreta" })
+            return
+        }
+
+        await getNewsletter()
+        formatFile()
+        await epubParser()
+        await sendNewsLetter()
+
+        res.status(200).json({ msg: "Newsletter enviada" })
     } catch (error) {
         res.status(500).json({ msg: "Erro interno, tente novamente" })
     }
