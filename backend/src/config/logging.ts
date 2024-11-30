e11a3733357572196085df78cfc1d067fc1fd657
Nicolas Klein <141509949+nicolaskleinaraujo@users.noreply.github.com>
2024-11-30 13:34:36 -0300

    adds timestamp format on logger

--- backend/src/config/logging.ts
+++ backend/src/config/logging.ts
@@ -1,11 +1,14 @@
 import winston from "winston"
 
 const logger = winston.createLogger({
-    format: winston.format.json(),
+    format: winston.format.combine(
+        winston.format.timestamp({ format: "DD-MM-YY" }),
+        winston.format.json(),
+    ),
+
     transports: [
         new winston.transports.File({ filename: "server.log" })
     ],
-    
 })
 
 export default logger
