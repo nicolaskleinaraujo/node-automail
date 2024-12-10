fdd9f7ef4273911fc029995f6d3c69f30267f763
Nicolas Klein <141509949+nicolaskleinaraujo@users.noreply.github.com>
2024-12-10 15:29:45 -0300

    creates the base 'online users' section

--- frontend/src/components/Welcome.tsx
+++ frontend/src/components/Welcome.tsx
@@ -2,6 +2,7 @@
 import { Card, CardHeader, CardDescription, CardContent } from "@/components/ui/card"
 import { Avatar, AvatarImage } from "@/components/ui/avatar"
 import { Button } from "@/components/ui/button"
+import { Badge } from "@/components/ui/badge"
 
 // Modules
 import React, { Dispatch, SetStateAction } from "react"
@@ -12,22 +13,28 @@ interface WelcomeProps {
 
 const Welcome: React.FC<WelcomeProps> = ({ setSteps }) => {
     return (
-        <Card className="flex flex-col justify-center items-center text-center w-11/12 max-w-md md:max-w-lg lg:max-w-xl">
-            <CardHeader>
-                <Avatar className="w-16 h-16 m-auto">
-                    <AvatarImage src="https://github.com/nicolaskleinaraujo.png" />
-                </Avatar>
+        <div className="flex flex-col justify-center items-center text-center w-11/12 max-w-md md:max-w-lg lg:max-w-xl">
+            <Card className="flex flex-col justify-center items-center text-center w-11/12 max-w-md md:max-w-lg lg:max-w-xl">
+                <CardHeader>
+                    <Avatar className="w-16 h-16 m-auto">
+                        <AvatarImage src="https://github.com/nicolaskleinaraujo.png" />
+                    </Avatar>
 
-                <CardDescription>
-                    Receba a Newsletter do <a href="https://filipedeschamps.com.br/newsletter" target="_blank" className="underline animate-pulse text-yellow-300">Filipe Deschamps</a> em sua conta Kindle
-                </CardDescription>
-            </CardHeader>
+                    <CardDescription>
+                        Receba a Newsletter do <a href="https://filipedeschamps.com.br/newsletter" target="_blank" className="underline animate-pulse text-yellow-300">Filipe Deschamps</a> em sua conta Kindle
+                    </CardDescription>
+                </CardHeader>
 
-            <CardContent className="flex flex-col gap-3 sm:flex-row">
-                <Button variant="outline" onClick={() => setSteps(1)}>Iniciar passo-a-passo</Button>
-                <Button variant="destructive" onClick={() => setSteps(4)}>Remover e-mail</Button>
-            </CardContent>
-        </Card>
+                <CardContent className="flex flex-col gap-3 sm:flex-row">
+                    <Button variant="outline" onClick={() => setSteps(1)}>Iniciar passo-a-passo</Button>
+                    <Button variant="destructive" onClick={() => setSteps(4)}>Remover e-mail</Button>
+                </CardContent>
+            </Card>
+
+            <Badge className="mt-3">
+                <span className="inline-flex w-2 h-2 mr-2 rounded-full bg-green-500 animate-pulse"></span> XX usuários ativos
+            </Badge>
+        </div>
     )
 }
 
